2151fb71355123a4dc0db1ace657207b86dc711d
dpx-k <deepakkrishna2206@gmail.com>
2025-02-17 22:48:12 +0530

    Added concatenation and logical Operators

---
+++ Basics/logicalOperators.js
@@ -0,0 +1,13 @@
+var currentTime = 10;
+console.log(currentTime > 9 && currentTime < 17);
+
+var currentTime = 7;
+console.log(currentTime < 9 || currentTime > 17);
+
+var petHungry = true
+console.log('Feeding the pet');
+console.log("Pet is hungry: ", !petHungry);
+console.log(petHungry);
+
+var petHungry = true;
+petHungry = !petHungry;
