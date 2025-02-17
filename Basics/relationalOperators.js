2f4d736e187fafcfecc6ec136ba9acacc2294094
dpx-k <deepakkrishna2206@gmail.com>
2025-02-17 22:07:12 +0530

    Added files on operators

---
+++ Basics/relationalOperators.js
@@ -0,0 +1,5 @@
+console.log(2 > 3)
+console.log(3 > 2)
+console.log(10 == 10)
+
+// returns boolean values 
---
+++ Basics/strictComparison.js
@@ -0,0 +1,11 @@
+console.log(100 == '100') // will return true as js will only check for the value and not the type
+
+// strict comparison operator with 3 equal signs check for the type too 
+
+console.log(100 === '100') // returns false 
+
+// similarly there is strict inequality operator too 
+
+console.log(1 != '1')
+
+console.log(1 !== '1')
