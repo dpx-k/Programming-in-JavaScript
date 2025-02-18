f7491f88ad695e72801d6d9a6e0812d19acdf7ae
dpx-k <deepakkrishna2206@gmail.com>
2025-02-18 20:09:20 +0530

    Learnt loops and nested loops

---
+++ 1.Basics/3.Loops/nestedloops.js
@@ -0,0 +1,10 @@
+for(var year = 2020; year <= 2025; year++){
+    console.log("Year:", year)
+    for(var month = 1; month <= 12; month++){
+        console.log("   Month:", month)
+        for(var day = 1; day <=30; day++){
+            console.log("       Day:", day)
+        }
+    }
+}
+
